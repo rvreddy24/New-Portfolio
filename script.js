c885a7a95ee2cca32ac92a3889e093185a06b523
Raj Varshith Reddy Malgireddy <52918908+rvreddy24@users.noreply.github.com>
2024-10-25 13:03:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function showDetails(projectId) {
+function showDetails(projectId, buttonId) {
     const details = document.getElementById(projectId);
-    details.style.display = details.style.display === "block" ? "none" : "block";
+    const button = document.getElementById(buttonId);
+
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+        button.innerHTML = '<span>Less Details</span> <i class="fas fa-chevron-up"></i>';
+    } else {
+        details.style.display = "none";
+        button.innerHTML = '<span>More Details</span> <i class="fas fa-chevron-right"></i>';
+    }
 }

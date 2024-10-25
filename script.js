89fb3269b1d8aa5929c2901b5951c3015a3b7e16
Raj Varshith Reddy Malgireddy <52918908+rvreddy24@users.noreply.github.com>
2024-10-25 09:33:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// Toggle project details visibility
 function showDetails(projectId) {
     const details = document.getElementById(projectId);
     if (details.style.display === "none" || details.style.display === "") {
         details.style.display = "block";
+        details.style.animation = "fadeIn 0.5s";
     } else {
         details.style.display = "none";
     }
 }
+

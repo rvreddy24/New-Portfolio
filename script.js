dfafc0ae14b95ef5749aabed62c64abe6f07de9e
Raj Varshith Reddy Malgireddy <52918908+rvreddy24@users.noreply.github.com>
2024-10-25 13:00:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function showDetails(projectId) {
     const details = document.getElementById(projectId);
-    details.style.display = (details.style.display === "none" || details.style.display === "") ? "block" : "none";
+    details.style.display = details.style.display === "block" ? "none" : "block";
 }
